4ada5c67dd7f01de4318e8593f4107c4641abfd2
현수 <azxcv1768@gmail.com>
2023-04-24 07:34:27 +0900

    [Bronze III] Title: 공 넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[101];
+int main(){
+    int n,m; cin >> n >> m;
+    for(int i=1;i<=n;i++){
+        arr[i]=0;
+    }
+    for(int i=0;i<m;i++){
+        int a,b,c; cin >> a >> b >> c;
+        for(int j=a;j<=b;j++){
+            arr[j]=c;
+        }
+    }
+    for(int i=1;i<=n;i++){
+        cout << arr[i] << " ";
+    }
+}
