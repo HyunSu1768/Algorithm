24e8254eca2a6b78c5001145f559df5f62d565ea
현수 <azxcv1768@gmail.com>
2023-04-27 21:09:47 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.c
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-int main(){
-    int h,m; scanf("%d %d",&h,&m);
-    int n; scanf("%d",&n);
-    printf("%d %d",((h+(m+n)/60)%24),(m+n)%60);
+int main()
+{
+    int h,m,t;
+    scanf("%d %d %d",&h,&m,&t);
+    printf("%d %d",(h+(m+t)/60)%24,(m+t)%60);
 }
