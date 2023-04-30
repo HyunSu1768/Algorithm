3e5252c8bc43dd578c5462a8f95617dbb0fcaa64
현수 <azxcv1768@gmail.com>
2023-04-30 16:38:32 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 104 ms, Memory: 8760 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    set<string> s;
+    int t; cin >> t;
+    int flag=0;
+    int result=0;
+    while(t--){
+        string a; cin >> a;
+        if(a=="ENTER"){
+            result+=s.size();
+            s.clear();
+        }
+        else{
+            s.insert(a);
+        }
+    }
+    result += s.size();
+    cout << result;
+}
