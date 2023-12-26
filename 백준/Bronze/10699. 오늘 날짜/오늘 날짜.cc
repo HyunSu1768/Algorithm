5036212241085971cf27ab38dfa56c7947b57239
현수 <azxcv1768@gmail.com>
2023-12-26 23:12:31 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    time_t timer;
+    struct tm *t;
+    timer = time(NULL);
+    t = localtime(&timer);
+
+    printf("%d-%d-%d",t->tm_year+1900, t->tm_mon+1,t->tm_mday);
+}
