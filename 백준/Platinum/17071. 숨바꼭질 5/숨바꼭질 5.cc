980feec51d66df91e41cd176181637b817636c7c
현수 <azxcv1768@gmail.com>
2024-01-18 11:59:15 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 12 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
@@ -1,45 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n,m;
-int visited[2][500003];
+
+int visited[2][500002];
+
 int main(){
-    cin >> n >> m;
-    if(n==m){
-        cout << "0";
-        return 0;
-    }
+    int n,m; cin >> n >> m;
     queue<int> Q;
     Q.push(n);
-    int b = m;
+    if(n==m) {
+        cout << "0"; return 0;
+    }
     int turn = 1;
     int ok = 0;
     while(!Q.empty()) {
-        b += turn;
-        if(b > 500000) {
-            break;
-        }
-        if(visited[turn%2][b]) {
+        m += turn;
+        if(m > 500000) break;
+        if(visited[turn%2][m]) {
             ok = 1;
             break;
         }
-
         int qsize = Q.size();
         for(int i=0;i<qsize;i++) {
             int x = Q.front(); Q.pop();
             for(int nx : {x-1,x+1,x*2}) {
                 if(nx < 0 || nx > 500000 || visited[turn%2][nx]) continue;
                 visited[turn%2][nx] = 1;
-                if(nx == b) {
+                if(nx == m) {
                     ok = 1;
                     break;
                 }
+                if(ok) break;
                 Q.push(nx);
             }
-            if(ok) break;
         }
         if(ok) break;
         turn++;
     }
-    if(ok) cout << turn;
+    if(ok) {
+        cout << turn;
+    }
     else cout << "-1";
 }
